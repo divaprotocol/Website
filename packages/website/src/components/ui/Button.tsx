1f98dbc775585134f15304723c6fcd51d2167aad
Julian Krispel-Samsel <julian@jkrsp.com>
2022-12-01 09:36:53 +0000

    wip

--- packages/website/src/components/ui/Button.tsx
+++ packages/website/src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react"
 export const Button = (props: PropsWithChildren<{ primary?: boolean }>) => {
   let innerClassName = /*tw*/ "px-8 py-4 [border-radius:2rem]";
   let className =
-    /*tw*/ "font-medium flex [border-radius:2rem] bg-transparent font-sans bg-gradient-to-l from-blue-700 to-teal-500 overflow-hidden [padding:2px]";
+    /*tw*/ "text-white font-medium flex [border-radius:2rem] bg-transparent font-sans bg-gradient-to-l from-blue-700 to-teal-500 overflow-hidden [padding:2px]";
   if (props.primary) {
     innerClassName +=
       /*tw*/ " bg-transparent";
