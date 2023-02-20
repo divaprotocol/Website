47bd4a99013d285e9fd3bcd622dededd899b4e91
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2023-02-20 18:55:59 +0530

    feat: reward page

--- packages/website/src/pages/rewards.tsx
+++ packages/website/src/pages/rewards.tsx
@@ -1,7 +1,7 @@
 import { useAppSelector } from '../redux/hooks'
 import { selectUserAddress } from '../redux/appSlice'
 import { useEffect, useState } from 'react'
-import { AlertIcon } from '@chakra-ui/react'
+import { Alert, AlertIcon, Stack } from '@chakra-ui/react'
 
 /**
  * TODO: load rewards via ajax
@@ -10,6 +10,49 @@ import PageLayout from '../components/pageLayout/PageLayout'
 import { ConnectWalletButton } from '../components/ConnectWalletButton'
 import { Heading } from '../components/typography/Heading'
 import { Paragraph } from '../components/typography/Paragraph'
+import { Highlight } from '../components/typography/Highlight'
+
+const RewardPageBlobs = () => (
+	<>
+		<div
+			style={{
+				position: 'absolute',
+				width: '726px',
+				height: '594px',
+				left: '1304px',
+				top: 'calc(50% - 594px/2 + 46px)',
+				background:
+					'linear-gradient(116.38deg, rgba(0, 56, 255, 0.2) 6.37%, rgba(22, 227, 216, 0.2) 89.66%)',
+				filter: 'blur(131.902px)',
+				transform: 'matrix(-1, 0, 0, 1, 0, 0)',
+			}}></div>
+		<div
+			style={{
+				position: 'absolute',
+				width: '402px',
+				height: '329px',
+				left: '0px',
+				top: 'calc(50% - 329px/2 + 93.5px)',
+				background:
+					'linear-gradient(116.38deg, rgba(0, 56, 255, 0.3) 6.37%, rgba(22, 227, 216, 0.3) 89.66%)',
+				filter: 'blur(131.902px)',
+				transform: 'matrix(-1, 0, 0, 1, 0, 0)',
+			}}></div>
+
+		<div
+			style={{
+				position: 'absolute',
+				width: '402px',
+				height: '329px',
+				left: '821px',
+				top: 'calc(50% - 329px/2 - 179.5px)',
+				background:
+					'linear-gradient(116.38deg, rgba(0, 56, 255, 0.3) 6.37%, rgba(22, 227, 216, 0.3) 89.66%)',
+				filter: 'blur(131.902px)',
+				transform: 'matrix(-1, 0, 0, 1, 0, 0)',
+			}}></div>
+	</>
+)
 
 const Rewards = () => {
 	const userAddress = useAppSelector(selectUserAddress)
@@ -37,12 +80,25 @@ const Rewards = () => {
 	}, [rewards, userAddress])
 	return (
 		<PageLayout>
-			<div>
+			<Stack
+				style={{
+					flexDirection: 'column',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					alignContent: 'center',
+					color: 'white',
+					textAlign: 'center',
+					zIndex: 10,
+				}}
+				height={['80vh']}
+				spacing={8}>
 				<div>
-					<Heading as="h1" size="lg">
-						$DIVA Token Claim
+					<Heading as="h2" size="lg">
+						$DIVA Token
+						<Highlight> Claim</Highlight>
 					</Heading>
-					<Paragraph>
+					<Paragraph className="mt-8">
 						$DIVA is the governance token for DIVA Protocol.
 					</Paragraph>
 				</div>
@@ -55,24 +111,39 @@ const Rewards = () => {
 				)}
 				{userAddress !== undefined && rewardInfo == null && (
 					<div>
-						{/* <AlertIcon /> */}
-						Connected account was not registered for the testnet
+						<Alert
+							status="error"
+							variant="subtle"
+							className="text-black rounded-xl">
+							<AlertIcon />
+							Connected account was not registered for the testnet
+						</Alert>
 					</div>
 				)}
 				{userAddress !== undefined &&
 					rewardInfo.reward !== undefined &&
 					rewardInfo.reward !== '' && (
 						<div>
-							{/* <AlertIcon /> */}
-							You are eligible for token claim, below are the details of your
-							participation.
+							<Alert
+								status="success"
+								variant="subtle"
+								className="text-black rounded-xl">
+								<AlertIcon />
+								You are eligible for token claim, below are the details of your
+								participation.
+							</Alert>
 						</div>
 					)}
 
 				{userAddress !== undefined && rewardInfo.reward === '' && (
 					<div>
-						{/* <AlertIcon /> */}
-						{'You are not eligible. Reason: ' + rewardInfo.comment}
+						<Alert
+							status="error"
+							variant="subtle"
+							className="text-black rounded-xl">
+							<AlertIcon />
+							{'You are not eligible. Reason: ' + rewardInfo.comment}
+						</Alert>
 					</div>
 				)}
 				{userAddress !== undefined &&
@@ -99,21 +170,32 @@ const Rewards = () => {
 					rewardInfo.reward !== undefined &&
 					rewardInfo.reward !== '' && (
 						<div>
-							{/* <AlertIcon /> */}
-							You will be able to claim your rewards once the token launches
+							<Alert
+								status="info"
+								variant="subtle"
+								className="text-black rounded-xl">
+								<AlertIcon />
+								You will be able to claim your rewards once the token launches
+							</Alert>
 						</div>
 					)}
 				{userAddress !== undefined && rewardInfo.reward === undefined && (
 					<div>
-						{/* <AlertIcon /> */}
-						<Paragraph>You were not registered</Paragraph>
+						<Alert
+							status="error"
+							variant="subtle"
+							className="text-black rounded-xl">
+							<AlertIcon />
+							<Paragraph>You were not registered</Paragraph>
+						</Alert>
 					</div>
 				)}
 
-				<div>
+				<div className="z-10">
 					<ConnectWalletButton />
 				</div>
-			</div>
+			</Stack>
+			<RewardPageBlobs />
 		</PageLayout>
 	)
 }
