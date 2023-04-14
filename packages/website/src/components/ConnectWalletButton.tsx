c8cfce300e89cf07cfb02296f3aa3221c1a7fbc9
Tarun yadav <tarunyadav9761@gmail.com>
2023-04-15 00:04:13 +0530

    upgrade typescript and added change network

--- packages/website/src/components/ConnectWalletButton.tsx
+++ packages/website/src/components/ConnectWalletButton.tsx
@@ -1,25 +1,40 @@
 import { getShortenedAddress } from '../util/getShortenedAddress'
 import { Button } from './ui/Button'
-import { useAccount } from 'wagmi'
-import { useConnectModal, useAccountModal } from '@rainbow-me/rainbowkit'
-import { Stack } from '@chakra-ui/react'
+import { useAccount, useNetwork } from 'wagmi'
+import {
+	useConnectModal,
+	useAccountModal,
+	useChainModal,
+} from '@rainbow-me/rainbowkit'
+import { Stack, useToast } from '@chakra-ui/react'
 
 export function ConnectWalletButton() {
 	const { address, isConnected } = useAccount()
 	const { openConnectModal } = useConnectModal()
 	const { openAccountModal } = useAccountModal()
+	const { openChainModal } = useChainModal()
+	const { chain } = useNetwork()
 
 	return (
 		<Stack flexDirection={'row'} justify={'center'} alignItems={'center'}>
-			<Button
-				primary={isConnected ? false : true}
-				onClick={isConnected ? openAccountModal : openConnectModal}
-				className="ml-4 justify-center items-center"
-				innerClassName="">
-				{isConnected
-					? getShortenedAddress(address as string)
-					: 'Connect Wallet'}
-			</Button>
+			{chain?.unsupported === true ? (
+				<Button
+					onClick={openChainModal}
+					className="ml-4 justify-center items-center bg-red-500 font-serif"
+					innerClassName="bg-[#ff494a]/30">
+					Wrong Network
+				</Button>
+			) : (
+				<Button
+					primary={isConnected ? false : true}
+					onClick={isConnected ? openAccountModal : openConnectModal}
+					className="ml-4 justify-center items-center font-serif"
+					innerClassName="">
+					{isConnected
+						? getShortenedAddress(address as string)
+						: 'Connect Wallet'}
+				</Button>
+			)}
 		</Stack>
 	)
 }
