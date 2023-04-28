6b9f98d12c9d57a38978b5dbc8f19fa736f06d10
Walodja1987 <wladimir.weinbender@gmail.com>
2023-04-28 15:39:45 +0200

    Remove redundant constants

--- packages/website/src/constants.ts
+++ packages/website/src/constants.ts
@@ -14,40 +14,10 @@ export enum SupportedChainId {
 
 export const config = {
   [SupportedChainId.MAINNET]: {
-    name: "Ethereum",
-    divaAddress: "",
-    divaAddressNew: "",
-    balanceCheckAddress: "",
-    exchangeProxy: "",
-    whitelistAddress: "",
-    divaSubgraph: "",
-    whitelistSubgraph: "",
-    allOrders: "",
-    order: "",
-    orderbook: "",
-    explorer: "https://etherscan.io/",
-    nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-    isSupported: false,
+    claimDivaLinearVestingAddress: "",
   },
   [SupportedChainId.GOERLI]: {
-    divaAddress: "0x8f138cfC5de71FCde7FdeCd87EAC6Aa6A536Bf85",
-    divaAddressNew: "0x6cDEc9b70431bf650f3A0DDD0e246368a4C4F1E1",
-    divaTokenAddress: "0xcA68992Db9d413eAd58Bd73e84E4997537C879F9",
     claimDivaLinearVestingAddress:
       "0x2758f311a61e84A3b4305962E10C9393d89128C8",
-    balanceCheckAddress: "0x9293ff9733AC7666A8251564C083191c3DA8BE19",
-    exchangeProxy: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",
-    whitelistAddress: "0x017aA6E15e406b85b8b1dF322e39444D819C8F43",
-    divaSubgraph:
-      "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-goerli",
-    whitelistSubgraph:
-      "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-whitelist-goerli",
-    allOrders: "https://eip712api.xyz/orderbook/v1/orders/",
-    order: "https://eip712api.xyz/orderbook/v1/order/",
-    orderbook: "https://eip712api.xyz/orderbook/v1",
-    explorer: "https://goerli.etherscan.io/",
-    name: "Görli",
-    nativeCurrency: { name: "Görli Ether", symbol: "görETH", decimals: 18 },
-    isSupported: true,
   },
 };
