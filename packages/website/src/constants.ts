c0a18463f60a59b23d4b689fd64523e35260f01d
Walodja1987 <wladimir.weinbender@gmail.com>
2023-04-28 15:16:38 +0200

    Addresses and claim lists updated

--- packages/website/src/constants.ts
+++ packages/website/src/constants.ts
@@ -34,7 +34,7 @@ export const config = {
     divaAddressNew: "0x6cDEc9b70431bf650f3A0DDD0e246368a4C4F1E1",
     divaTokenAddress: "0xcA68992Db9d413eAd58Bd73e84E4997537C879F9",
     claimDivaLinearVestingAddress:
-      "0x0bA350467a397A1aC133cbFe73BF5050E6Bee12b",
+      "0x2758f311a61e84A3b4305962E10C9393d89128C8",
     balanceCheckAddress: "0x9293ff9733AC7666A8251564C083191c3DA8BE19",
     exchangeProxy: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",
     whitelistAddress: "0x017aA6E15e406b85b8b1dF322e39444D819C8F43",
