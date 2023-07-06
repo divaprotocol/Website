532cfac659f6695db8f4bb2635955a58de08cf85
Tarun yadav <tarunyadav9761@gmail.com>
2023-07-06 21:01:33 +0530

    changes for dex

--- packages/website/src/constants/index.ts
+++ packages/website/src/constants/index.ts
@@ -1,13 +1,15 @@
 const constants = {
     discordLink: "https://discord.com/invite/DE5b8ZeJjK",
     twitterLink: "https://twitter.com/divaprotocol_io",
-    githubLink: "https://github.com/divaprotocol",
+    githubLink: "https://github.com/divaprotocol/diva-protocol-v1",
+    mailLink: "mailto:vlad@divaprotocol.io",
 
     documentationUrl: "https://docs.divaprotocol.io/",
     appUrl: "https://app.diva.finance/",
     peckShieldAudit: 'https://docs.divaprotocol.io/for-developers/technical-resources',
     divaDonate: "https://www.divadonate.xyz/",
     divaViz: "https://divaviz.com/",
+    divaTokenAddress: '0x4B7fFCB2b92fB4890f22f62a52Fb7A180eaB818e',
 }
 
 export default constants;
